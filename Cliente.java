51ef201c30e55205f22359b19845852d40e0e19b
Lorena Cardozo <lorenasociallife@gmail.com>
2026-07-10 02:41:57 -0300

    Cliente.java
    Cliente é a classe que representa o cliente da imobiliária (nome, CPF, telefone, e-mail), com validações no cadastro e equals()/hashCode() sobrescritos para considerar dois clientes iguais quando têm o mesmo CPF.

---
+++ Cliente.java
@@ -0,0 +1,70 @@
+package modelo;
+
+import java.util.Objects;
+
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    public Cliente(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{nome='" + nome + "', cpf='" + cpf + "', telefone='" + telefone
+                + "', email='" + email + "'}";
+    }
+}
